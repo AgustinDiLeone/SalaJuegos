2210b9e584a91f6580be0e4ac3350a0ffafeee66
AgustinDiLeone <agustindileone@gmail.com>
2024-09-10 15:43:35 -0300

    crear componente home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,10 @@
 import { Routes } from '@angular/router';
-import { AuthComponent } from './componentes/auth/auth.component';
 
 export const routes: Routes = [
 
     {
         path: "",
-        redirectTo: "auth",
+        redirectTo: "index.html",
         pathMatch: "full"
     },
-    {
-        path: "auth",
-        component: AuthComponent
-    }
 ];
