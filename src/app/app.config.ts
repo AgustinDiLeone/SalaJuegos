1a4611ef5725170eda3e23d55e9f67adfd3f8cde
AgustinDiLeone <agustindileone@gmail.com>
2024-09-12 10:56:09 -0300

    firebase implementado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,27 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'dileonesalajuegos',
+        appId: '1:567745534532:web:51482d2ca99a4ca0463a11',
+        storageBucket: 'dileonesalajuegos.appspot.com',
+        apiKey: 'AIzaSyDur3wO0qmTHJ1evp77G52AFu4YHf2t08s',
+        authDomain: 'dileonesalajuegos.firebaseapp.com',
+        messagingSenderId: '567745534532',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ],
 };
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,4 +11,27 @@ import { RouterLink } from '@angular/router';
 })
 export class LoginComponent {
 
+  form = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required, Validators.minLength(6)])
+  })
+  ingresar(){}
+  ingresarAdmin(){
+    const correo = 'admin@gmail.com';
+    this.form.controls.email.setValue(correo);
+    const password = 'Admin1234';
+    this.form.controls.password.setValue(password);
+  }
+  ingresarProp(){
+    const correo = 'agustindileone@gmail.com';
+    this.form.controls.email.setValue(correo);
+    const password = 'Agus1234';
+    this.form.controls.password.setValue(password);
+  }
+  ingresarUser(){
+    const correo = 'user@gmail.com';
+    this.form.controls.email.setValue(correo);
+    const password = 'User1234';
+    this.form.controls.password.setValue(password);
+  }
 }
